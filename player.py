264a51d7a81f5c40aabd9244a085dc5d0d4ddcab
siposzoltan03 <sipos.zoltan03@gmail.com>
2019-12-09 14:00:02 +0100

    2.0

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 class Player:
-    VERSION = "1.9"
+    VERSION = "2.0"
 
     def betRequest(self, game_state):
         current_buyin = (game_state["current_buy_in"])
@@ -27,12 +27,17 @@ class Player:
             if comm_cards and (hole_cards[0]["rank"] == hole_cards[1]["rank"]
                                or hole_cards[0]["rank"] in [comm_cards[i]["rank"] for i in range(3)]
                                or hole_cards[1]["rank"] in [comm_cards[i]["rank"] for i in range(3)]):
-                return min( current_buyin - game_state["players"][in_action]["bet"] + minimum_raise * 2, game_state["players"][in_action]["stack"] / 2)
+                return min(current_buyin - game_state["players"][in_action]["bet"] + minimum_raise * 2, game_state["players"][in_action]["stack"] / 2)
             return min(current_buyin - game_state["players"][in_action]["bet"] + minimum_raise, game_state["players"][in_action]["stack"] / 2)
         elif len(comm_cards) == 3:
             all_cards = [comm_cards[0]["suit"], comm_cards[1]["suit"], comm_cards[2]["suit"], hole_cards[0]["suit"], hole_cards[1]["suit"]]
-            if len(set(all_cards)) < 3:
+            rank_cards = [comm_cards[0]["rank"], comm_cards[1]["rank"], comm_cards[2]["rank"], hole_cards[0]["rank"], hole_cards[1]["rank"]]
+            if len(set(all_cards)) < 3 or len(set(rank_cards)) <= 3:
                 return game_state["players"][in_action]["stack"]
+        elif len(comm_cards) == 4:
+
+        elif len(comm_cards) == 5:
+
         else:
             print(hole_cards, sys.stderr)
 
