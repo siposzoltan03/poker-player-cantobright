0c75acbbbc414b236c9ad1f01b70b609b2574945
siposzoltan03 <sipos.zoltan03@gmail.com>
2019-12-09 10:51:07 +0100

    second commit

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 
 import json
+import sys
 
 class Player:
     VERSION = "1.0"
@@ -7,11 +8,13 @@ class Player:
     def betRequest(self, game_state):
         current_buyin = (game_state["current_buy_in"])
         small_blind = game_state["small_blind"]
+        in_action = game_state["in_action"]
+
         if current_buyin > small_blind * 2:
             return 0
         else:
-            # print(game_state[""])
-            return current_buyin - game_state["players"]["in_action"]["bet"]
+            # print(message, file=sys.stderr)
+            return current_buyin - game_state["players"][in_action]["bet"]
 
     def showdown(self, game_state):
         pass
